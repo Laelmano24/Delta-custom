74e28b6e1a757f822c39d5d038450ccc56d25e69
Laelmano24 <brockbomba@gmail.com>
2025-01-09 11:11:15 -0300

    update init.lua

--- Templates/Sonic Template/init.lua
+++ Templates/Sonic Template/init.lua
@@ -7,17 +7,17 @@ shared.DeltaGuiInterfaceColors = {
     BorderThickness = 3
   },
   slideBar = {
-    SildeBarColor = Color3.fromRGB(0, 15, 90),
+    SildeBarColor = Color3.fromRGB(0, 11, 64),
     ActiveColor = Color3.fromRGB(1, 40, 232),
     InactiveColor = Color3.fromRGB(1, 29, 172)
   },
   MainExecutor = {
-    BackgroundColor = Color3.fromRGB(0, 15, 90),
+    BackgroundColor = Color3.fromRGB(0, 11, 64),
     SideMenu = {
-      BackgroundColor = Color3.fromRGB(0, 15, 90),
+      BackgroundColor = Color3.fromRGB(0, 11, 64),
       BackgroundImageColor = Color3.fromRGB(1, 29, 172),
       TextInfoColor = Color3.fromRGB(255, 255, 255),
-      TextInfoFontColor = "#4A00FF"
+      TextInfoFontColor = "#0128E8"
     },
     Buttons = {
       ExecuteTextColor = Color3.fromRGB(1, 40, 232),
@@ -40,16 +40,16 @@ shared.DeltaGuiInterfaceColors = {
   },
   MainHome = {
     Searchbar = {
-      BackgroundColor = Color3.fromRGB(0, 15, 90),
+      BackgroundColor = Color3.fromRGB(0, 11, 64),
       BackgroundButtonColor = Color3.fromRGB(1, 40, 232)
     },
     Holder = {
-      BackgroundColor = Color3.fromRGB(0, 15, 90),
+      BackgroundColor = Color3.fromRGB(0, 11, 64),
       BackgroundButtonColor = Color3.fromRGB(0, 33, 183),
       FrameBackgroundColor = Color3.fromRGB(1, 40, 232)
     },
     Popup = {
-      BackgroundColor = Color3.fromRGB(0, 15, 90),
+      BackgroundColor = Color3.fromRGB(0, 11, 64),
       BackgroundButtonColor = Color3.fromRGB(1, 40, 232),
       InputTextColor = Color3.fromRGB(255, 255, 255),
       InputBackgroundColor = Color3.fromRGB(54, 28, 113),
@@ -58,14 +58,14 @@ shared.DeltaGuiInterfaceColors = {
   },
   MainScriptHub = {
     Searchbar = {
-      BackgroundColor = Color3.fromRGB(0, 15, 90),
+      BackgroundColor = Color3.fromRGB(0, 11, 64),
     },
     Holder = {
-      BackgroundColor = Color3.fromRGB(0, 15, 90),
+      BackgroundColor = Color3.fromRGB(0, 11, 64),
       VerifiedFrame = Color3.fromRGB(91, 48, 193)
     },
     Popup = {
-      BackgroundColor = Color3.fromRGB(0, 15, 90),
+      BackgroundColor = Color3.fromRGB(0, 11, 64),
       ExecuteTextColor = Color3.fromRGB(1, 40, 232),
       ExecuteBorderColor = Color3.fromRGB(1, 34, 197),
       ExecuteBackground = Color3.fromRGB(1, 30, 173),
@@ -75,7 +75,7 @@ shared.DeltaGuiInterfaceColors = {
     },
   },
   MainConsole = {
-    BackgroundColor = Color3.fromRGB(0, 15, 90),
+    BackgroundColor = Color3.fromRGB(0, 11, 64),
     BackgroundConsole = Color3.fromRGB(12, 6, 26),
     SearchbarColor = Color3.fromRGB(1, 40, 232),
     ClearTextColor = Color3.fromRGB(1, 40, 232),
@@ -84,23 +84,23 @@ shared.DeltaGuiInterfaceColors = {
   },
   MainSettings = {
     Searchbar = {
-      BackgroundColor = Color3.fromRGB(0, 15, 90)
+      BackgroundColor = Color3.fromRGB(0, 11, 64)
     },
     Holder = {
       Button = {
-        BackgroundColor = Color3.fromRGB(0, 15, 90),
+        BackgroundColor = Color3.fromRGB(0, 11, 64),
         DescriptionColor = Color3.fromRGB(255, 255, 255),
         ClickButton = Color3.fromRGB(91, 48, 193)
       },
       Toggle = {
         ToggleBackgroundEnabled = Color3.fromRGB(71, 34, 146),
-        ToggleBackgroundDisabled = Color3.fromRGB(0, 15, 90),
+        ToggleBackgroundDisabled = Color3.fromRGB(0, 11, 64),
         ToggleIconEnabled = Color3.fromRGB(113, 55, 234),
         ToggleIconDisabled = Color3.fromRGB(51, 25, 106),
         DescriptionColor = Color3.fromRGB(255, 255, 255),
       },
       ButtonDropdown = {
-        BackgroundColor = Color3.fromRGB(0, 15, 90),
+        BackgroundColor = Color3.fromRGB(0, 11, 64),
         Title = Color3.fromRGB(255, 255, 255),
         Description = Color3.fromRGB(255, 255, 255),
         Dropdown = {
@@ -111,7 +111,7 @@ shared.DeltaGuiInterfaceColors = {
       }
     },
     Sort = {
-      BackgroundColor = Color3.fromRGB(0, 15, 90),
+      BackgroundColor = Color3.fromRGB(0, 11, 64),
       BackgroundButtonColor = Color3.fromRGB(71, 35, 148)
     }
   }
